515e0643568e36ca415f9d957922279e233e1dc9
edouard doin <edouarddoin@gmail.com>
2022-05-20 17:03:18 +0200

    watch list 3"

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :list_id, uniqueness: true
+  validates :list_id, presence: true
   validates :comment, length: { minimum: 5 }
   validates :movie_id, uniqueness: { scope: :list_id }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: "lists#index"
   resources :lists do
-    resources :bookmarks
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: :destroy
 end
